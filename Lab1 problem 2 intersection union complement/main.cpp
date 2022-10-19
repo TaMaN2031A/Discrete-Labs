a0db93778678f04228b2c58f48c3d01823203efc
Abdullah Taman <99008460+TaMaN2031A@users.noreply.github.com>
2022-10-19 16:03:18 +0200

    Update main.cpp
    Full solution to the problem.

--- Lab1 problem 2 intersection union complement/main.cpp
+++ Lab1 problem 2 intersection union complement/main.cpp
@@ -1,155 +1,222 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#define play long long
-#define go char
-#define ok bool
-#define fire cout
+
 using namespace std;
-play hashing[1000][1000];
-map <string, play> dict1;
-map <play, string> dict2;
-play numberOfElements, numberOfSits;
+long long hashing[1000][1000];
+map <string, long long> dict1;
+map <long long, string> dict2;
+long long numberOfElements, numberOfSits;
 
 void unions()
 {
-    fire << "Please insert the number of first set you want to union" << endl;
-    play first;
+    cout << "Please insert the number of first set you want to union" << endl;
+    long long first;
     cin >> first;
-    fire << "Please insert the number of second set you want to union" << endl;
-    play second;
+    cout << "Please insert the number of second set you want to union" << endl;
+    long long second;
     cin >> second;
     first--, second--;
-    ok tmam = false;
-    fire << "{";
-    play counterforprinting = 0;
+    bool tmam = false;
+    cout << "{";
+    long long counterforprinting = 0;
     for(int i = 0; i < numberOfElements; i++)
     {
-        play x = hashing[first][i] | hashing[second][i];
+        long long x = hashing[first][i] | hashing[second][i];
         if(x == 1)
         {
             counterforprinting++;
             if(counterforprinting != 1)
                 cout << ", ";
-            fire << dict2[i];
+            cout << dict2[i];
             tmam = true;
         }
     }
-    fire << "}" << endl;
+    cout << "}" << endl;
     if(!tmam)
-        fire << "Inserted sets doesn't exist" << endl;
+        cout << "Inserted sets doesn't exist" << endl;
     cout << endl;
 }
 void intersect()
 {
-    fire << "Please insert the number of first set you want to intersect" << endl;
-    play first;
+    cout << "Please insert the number of first set you want to intersect" << endl;
+    long long first;
     cin >> first;
-    fire << "Please insert the number of second set you want to intersect" << endl;
-    play second;
+    cout << "Please insert the number of second set you want to intersect" << endl;
+    long long second;
     cin >> second;
     first--, second--;
-    ok tmam = false;
-    fire << "{";
-    play counterforprinting = 0;
+    bool tmam = false;
+    cout << "{";
+    long long counterforprinting = 0;
     for(int i = 0; i < numberOfElements; i++)
     {
-        play x = hashing[first][i] & hashing[second][i];
+        long long x = hashing[first][i] & hashing[second][i];
         if(x == 1)
         {
             counterforprinting++;
             if(counterforprinting != 1)
                 cout << ", ";
-            fire << dict2[i];
+            cout << dict2[i];
             tmam = true;
         }
     }
-    fire << "}" << endl;
+    cout << "}" << endl;
     if(!tmam)
-        fire << "Inserted sets doesn't exist" << endl;
+        cout << "Inserted sets doesn't exist" << endl;
     cout << endl;
 }
 void complement()
 {
-    fire << "Please insert the number of set you want to complement" << endl;
-    play first;
+    cout << "Please insert the number of set you want to complement" << endl;
+    long long first;
     cin >> first;
     first--;
-    ok tmam = false;
-    fire << "{";
-    play counterforprinting = 0;
+    bool tmam = false;
+    cout << "{";
+    long long counterforprinting = 0;
     for(int i = 0; i < numberOfElements; i++)
     {
-        play x = 1-hashing[first][i];
+        long long x = 1-hashing[first][i];
         if(x == 1)
         {
             counterforprinting++;
             if(counterforprinting != 1)
                 cout << ", ";
-            fire << dict2[i];
+            cout << dict2[i];
             tmam = true;
         }
     }
-    fire << "}" << endl;
+    cout << "}" << endl;
     if(!tmam)
-        fire << "Inserted sets doesn't exist" << endl;
+        cout << "Inserted sets doesn't exist" << endl;
     cout << endl;
 }
-int main()
+
+int getbit(int number,int pos){
+
+    int x;
+
+    x=((1<<pos)&number);
+if(x!=0){
+    x=1;
+}
+
+    return x;
+
+}
+
+int setbit(int number,int pos){
+
+    int x;
+    x=((1<<pos)|number);
+    return x;
+
+}
+int clearbit(int number,int pos){
+
+    int x;
+    x=((~(1<<pos))&number);
+    return x;
+
+}
+
+int count_ones(unsigned int number){
+
+    int count=0;
+
+    while (number!=0){
+
+        if((number&(1))!=0){
+            count++;
+        }
+        number=number>>1;
+
+    }
+
+    return count;
+}
+int find_single_number(){
+
+    int n,number=0,f=0,a[100000];
+    cin>>n;
+    for (int i = 0; i <  n; ++i) {
+        cin>>a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        number=number^a[i];
+    }
+    return number;
+
+}
+int updatebit(int number,int pos,bool value){
+int x;
+ if(value){
+     x = setbit(number,pos);
+ }
+ else {
+     x = clearbit(number,pos);
+ }
+    return x;
+
+}
+void problem2()
 {
-    fire << "Please Enter the number of elements in the universal set" << endl;
+
+    cout << "Please Enter the number of elements in the universal set" << endl;
     cin >> numberOfElements;
-    for(play i = 0; i < numberOfElements; i++)
+    for(long long i = 0; i < numberOfElements; i++)
     {
         if(i==0)
-            fire << "Please Insert the " << i+1 << "st element";
+            cout << "Please Insert the " << i+1 << "st element";
         else if(i==1)
-            fire << "Please Insert the " << i+1 << "nd element";
+            cout << "Please Insert the " << i+1 << "nd element";
         else if(i==2)
-            fire << "Please Insert the " << i+1 << "rd element";
+            cout << "Please Insert the " << i+1 << "rd element";
         else
-            fire << "Please Insert the " << i+1 << "th element";
-        fire << endl;
+            cout << "Please Insert the " << i+1 << "th element";
+        cout << endl;
         string x;
         cin >> x;
         dict1[x] = i;
         dict2[i] = x;
     }
-    fire << "Ok... Now, please insert the number of subsets" << endl;
+    cout << "bool... Now, please insert the number of subsets" << endl;
     cin >> numberOfSits;
-    for(play i = 0; i < numberOfSits; i++)
+    for(long long i = 0; i < numberOfSits; i++)
     {
-        play setSize;
+        long long setSize;
         if(i==0)
-            fire << "Please insert the number of elements in the " << i + 1 << "st set";
+            cout << "Please insert the number of elements in the " << i + 1 << "st set";
         else if(i==1)
-            fire << "Please insert the number of elements in the " << i + 1 << "nd set";
+            cout << "Please insert the number of elements in the " << i + 1 << "nd set";
         else if(i==2)
-            fire << "Please insert the number of elements in the " << i + 1 << "rd set";
+            cout << "Please insert the number of elements in the " << i + 1 << "rd set";
         else
-            fire << "Please insert the number of elements in the " << i + 1 << "th set";
-        fire << endl;
+            cout << "Please insert the number of elements in the " << i + 1 << "th set";
+        cout << endl;
         cin >> setSize;
-        for(play j = 0; j < setSize; j++)
+        for(long long j = 0; j < setSize; j++)
         {
             if(j==0)
-                fire << "Please Insert the " << i+1 << "st element";
+                cout << "Please Insert the " << i+1 << "st element";
             else if(j==1)
-                fire << "Please Insert the " << i+1 << "nd element";
+                cout << "Please Insert the " << i+1 << "nd element";
             else if(j==2)
-                fire << "Please Insert the " << i+1 << "rd element";
+                cout << "Please Insert the " << i+1 << "rd element";
             else
-                fire << "Please Insert the " << i+1 << "th element";
+                cout << "Please Insert the " << i+1 << "th element";
             string buffer;
             cin >> buffer;
             hashing[i][dict1[buffer]] = 1;
         }
     }
-    fire << "Great! Now please insert the whatever you want to do" << endl;
-    fire << "If you want to union sets press 1" << endl;
-    fire << "If you want to intersect sets press 2" << endl;
-    fire << "If you want to complement a set press 3" << endl;
-    fire << "If you want to exit press 4" << endl;
-    play choice;
+    cout << "Great! Now please insert the whatever you want to do" << endl;
+    cout << "If you want to union sets press 1" << endl;
+    cout << "If you want to intersect sets press 2" << endl;
+    cout << "If you want to complement a set press 3" << endl;
+    cout << "If you want to exit press 4" << endl;
+    long long choice;
     cin >> choice;
     while(choice != 4)
     {
@@ -161,14 +228,64 @@ int main()
             complement();
         else
         {
-            fire << "Stop playing, enter a right choice" << endl;
+            cout << "Stop long longing, enter a right choice" << endl;
         }
-        fire << "If you want to union sets press 1" << endl;
-        fire << "If you want to intersect sets press 2" << endl;
-        fire << "If you want to complement a set press 3" << endl;
-        fire << "If you want to exit press 4" << endl;
+        cout << "If you want to union sets press 1" << endl;
+        cout << "If you want to intersect sets press 2" << endl;
+        cout << "If you want to complement a set press 3" << endl;
+        cout << "If you want to exit press 4" << endl;
         cin >> choice;
     }
-    fire << "Thank you" << endl;
+}
+void problem1()
+{
+    int choice;
+    cout << "insert the number of part you want to solve" << endl;
+    cin >> choice;
+    cout << "insert the number you want to process and the position" << endl;
+    int num, pos;
+    cin >> num >> pos;
+    if(choice == 1)
+        cout << getbit(num, pos);
+    else if(choice == 2)
+        cout << setbit(num, pos);
+    else if(choice == 3)
+        cout << clearbit(num, pos);
+    else if(choice == 4){
+        int op;
+        cout << "Enter the opartion you want" << endl;
+        cin >> op;
+        bool x;
+        if(op==0) x = false;
+        else x = true;
+        cout << updatebit(num, pos, op);
+    }
+    cout << endl;
+}
+void problem3()
+{
+    int choice;
+    cout << "insert the number of part you want to solve" << endl;
+    cin >> choice;
+    if(choice == 1){
+        unsigned int x;
+        cin >> x;
+        cout << count_ones(x);
+    }
+    else if(choice == 2)
+        cout << find_single_number();
+}
+int main()
+{
+    int choice;
+    cout << "insert the number of problem you want to solve" << endl;
+    cin >> choice;
+    if(choice == 1)
+        problem1();
+    else if(choice == 2)
+        problem2();
+    else if(choice == 3)
+        problem3();
+    cout << "Thank you" << endl;
     return 0;
 }
